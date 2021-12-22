1651db1ec5f6cc0bcdb3253f9634cd0e98783b96
OEM Configuration (temporary user) <sushmithakumaravel11@gmail.com>
2021-12-22 08:56:34 +0530

    testin and folder structure

---
+++ src/test/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './components/App';
+
+test("renders the app",()=> {
+  render(<App/>);
+  const scalerElement = screen.getByTitle(/scaler/i);
+  expect(scalerElement).toBeInTheDocument();
+
+});
